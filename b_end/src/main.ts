0b127cfd427d1daca957b3e9d05b7ad87d2fa070
youngpar <youngseo321@gmail.com>
2023-03-20 17:23:49 +0900

    [BE: FEAT] API Documentaion Setting

--- b_end/src/main.ts
+++ b_end/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const apiConfig = new DocumentBuilder()
+    .setTitle('A-')
+    .setDescription('Api Documentaion')
+    .setVersion('1.0')
+    .setContact('youngmon', '/', 'zsh@duck.com')
+    .build();
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(app, apiConfig),
+  );
   await app.listen(3000);
 }
 bootstrap();
