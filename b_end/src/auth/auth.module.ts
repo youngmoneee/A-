e067e41a89de01d48e4ae9e1cfff83a3863bea63
youngpar <youngseo321@gmail.com>
2023-03-23 15:54:07 +0900

    [BE: FEAT] KAKAO 유저 정보 GET

--- b_end/src/auth/auth.module.ts
+++ b_end/src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { KakaoStrategy } from './strategy/kakao.strategy';
 
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'kakao',
+      strategies: [KakaoStrategy],
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, KakaoStrategy],
 })
 export class AuthModule {}
---
+++ b_end/src/dto/enum.provider.ts
@@ -0,0 +1,4 @@
+export enum OauthProvider {
+  KAKAO = 'kakao',
+  GOOGLE = 'google',
+}
