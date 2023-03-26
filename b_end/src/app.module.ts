06c8c43edd1b60be61bf9cfe21ba44274cd623df
youngpar <youngseo321@gmail.com>
2023-03-26 22:09:37 +0900

    [BE: FEAT] Mqtt test 패턴 메세지 받으면 출력

--- b_end/src/app.module.ts
+++ b_end/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DevConfigModule } from './config/dev.config.module';
 import { AuthModule } from './auth/auth.module';
+import { MqttModule } from './mqtt/mqtt.module';
 
 @Module({
-  imports: [UserModule, DevConfigModule, AuthModule],
+  imports: [UserModule, DevConfigModule, AuthModule, MqttModule],
   controllers: [AppController],
   providers: [AppService],
 })
