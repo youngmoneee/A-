3eacf340cfc39c3c173f156f5fde6dc25af36e26
youngpar <youngseo321@gmail.com>
2023-04-12 23:59:59 +0900

    [INFRA: BUG] CORS 수정

--- f_end/src/components/login/GoogleButton.vue
+++ f_end/src/components/login/GoogleButton.vue
@@ -1,13 +1,15 @@
 <template>
 <div style="display: flex; flex-direction: column; align-items: center">
+  <a href = '/api/oauth/google'>
   <img
     src="@/assets/btn_google_signin_light_normal_web@2x.png"
     height="92"
     width="382"
   />
+  </a>
 </div>
 </template>
-<script>
+<script setup>
 </script>
 
 <style scoped>
