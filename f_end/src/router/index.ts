c1b6493e8fa342661731aa634b24a7beac8b9b3c
youngpar <youngseo321@gmail.com>
2023-05-03 19:51:54 +0900

    [INFRA: FEAT] Dockerfile Modified

--- f_end/src/router/index.ts
+++ f_end/src/router/index.ts
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import KakaoButton from '@/components/login/KakaoButton.vue';
+import GoogleButton from '@/components/login/GoogleButton.vue';
 
-const routes: Array<RouteRecordRaw> = [];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/api/oauth/kakao',
+    name: 'kakaoLogin',
+    component: KakaoButton,
+  },
+  {
+    path: '/api/oauth/google',
+    name: 'googleLogin',
+    component: GoogleButton,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
