cb3bb17898edb05dd8cf41974e5d2f1689a3a723
youngpar <youngseo321@gmail.com>
2023-05-23 23:57:16 +0900

    [FE: FEAT] Redirect / with Token

--- f_end/src/App.vue
+++ f_end/src/App.vue
@@ -8,7 +8,18 @@
 <script setup>
 import SideNav from '@/components/SideBar.vue';
 import LoginModal from '@/components/login/LoginModal.vue';
+import { useAuthStore } from '@/store/auth';
+import { onMounted } from 'vue';
 
+const authStore = useAuthStore();
+onMounted(() => {
+  const token = document.cookie.split(';')
+    .map((kv) => kv.split('='))
+      .map(([k, v]) => [k.trim(), v.trim()])
+    .find(([k]) => k === 'DEV_TOKEN');
+  authStore.token = token ? token[1] : null;
+  authStore.logToken();
+});
 </script>
 <style>
 #app {
--- f_end/src/components/login/KakaoButton.vue
+++ f_end/src/components/login/KakaoButton.vue
@@ -1,12 +1,10 @@
 <template>
 <div style="display: flex; flex-direction: column; align-items: center">
-<a href='/api/oauth/kakao'>
   <img
     src="@/assets/kakao_login_large_narrow.png"
     height="92"
     width="382"
   />
-</a>
 </div>
 </template>
 <script setup>
