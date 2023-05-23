ae19a952002f59878776d91431a336cc0dea2ade
youngpar <youngseo321@gmail.com>
2023-05-24 00:29:21 +0900

    [FE: FEAT] Button Remake

--- f_end/src/components/login/KakaoButton.vue
+++ f_end/src/components/login/KakaoButton.vue
@@ -1,10 +1,12 @@
 <template>
 <div style="display: flex; flex-direction: column; align-items: center">
+  <a href = '/api/oauth/kakao'>
   <img
     src="@/assets/kakao_login_large_narrow.png"
     height="92"
     width="382"
   />
+  </a>
 </div>
 </template>
 <script setup>
