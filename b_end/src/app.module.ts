73c8427d6c446499915978a963a1ceb05866b587
youngpar <youngseo321@gmail.com>
2023-06-05 03:56:09 +0900

    [BE: CHORE] Install @nestjs/socket.io @nestjs/websockets

--- b_end/src/app.module.ts
+++ b_end/src/app.module.ts
@@ -5,7 +5,6 @@ import { UserModule } from './user/user.module';
 import { DevConfigModule } from './config/dev.config.module';
 import { AuthModule } from './auth/auth.module';
 import { MqttModule } from './mqtt/mqtt.module';
-import { FileUploadModule } from './file_upload/file_upload.module';
 import { ChatModule } from './chat/chat.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -15,7 +14,6 @@ import { MongooseModule } from '@nestjs/mongoose';
     DevConfigModule,
     AuthModule,
     MqttModule,
-    FileUploadModule,
     ChatModule,
     MongooseModule.forRoot('mongodb://localhost:27017'),
   ],
