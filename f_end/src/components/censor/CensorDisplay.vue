f89bfa4a2d3065aaeecd1ebb9408fee370c072b0
youngpar <youngseo321@gmail.com>
2023-06-22 17:29:58 +0900

    [FE: FEAT] Implemented Layer of Censor Display

---
+++ f_end/src/components/censor/CensorDisplay.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='censor-display'>
+    센서 데이터 표시 위치
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.censor-display {
+  background: green;
+  height: 100%;
+}
+</style>
