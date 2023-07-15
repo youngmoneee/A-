0e4b13c3d27adf43f516b2a861e80cb70defa380
youngpar <youngseo321@gmail.com>
2023-07-16 03:16:43 +0900

    [BE: FEAT] Add API for Device

--- b_end/src/mqtt/mqtt.controller.ts
+++ b_end/src/mqtt/mqtt.controller.ts
@@ -1,18 +1,44 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { MqttService } from './mqtt.service';
 import { Response } from 'express';
+import { GetUser } from '../user/user.decorator';
+import { JwtGuard } from '../auth/guard/jwt.guard';
 @Controller('mqtt')
+@UseGuards(JwtGuard)
 export class MqttController {
   constructor(private readonly mqttService: MqttService) {}
 
-  @Get('topic')
-  topics() {
-    return this.mqttService.topicList();
+  @Get('device')
+  async deviceList(@GetUser() user) {
+    return await this.mqttService.deviceNames(user.id);
   }
-  @Post('topic')
-  register(@Res() res: Response, @Body('topic') topic) {
-    this.mqttService.topicRegister(topic);
-    //  TODO : FE로부터 입력받는 상황 고려해 WS || Controller에도 등록필요
-    return res.sendStatus(201);
+  @Post('device')
+  async register(@GetUser() user, @Body('device') device) {
+    await this.mqttService.deviceRegister(user.id, device);
+  }
+
+  @Get('device/:id')
+  deviceDetail(@Param('id', ParseIntPipe) id: number) {
+    return this.mqttService.getDeviceInfo(id);
+  }
+
+  @Get('on')
+  ir(@Res() res: Response) {
+    this.mqttService.publishMessage('dev1/on', '');
+    return res.sendStatus(200);
+  }
+  @Get('off')
+  iroff(@Res() res: Response) {
+    this.mqttService.publishMessage('dev1/off', '');
+    return res.sendStatus(200);
   }
 }
--- b_end/src/mqtt/mqtt.service.ts
+++ b_end/src/mqtt/mqtt.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import * as mqtt from 'mqtt';
 import { MqttGateway } from './mqtt.gateway';
 import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class MqttService implements OnModuleInit {
@@ -10,6 +11,7 @@ export class MqttService implements OnModuleInit {
   constructor(
     private readonly mqttGateway: MqttGateway,
     private readonly configService: ConfigService,
+    private readonly prismaService: PrismaService,
   ) {}
   onModuleInit() {
     this.client = mqtt.connect(
@@ -17,34 +19,106 @@ export class MqttService implements OnModuleInit {
         this.configService.get('MQTT_HOST') +
         ':' +
         this.configService.get('MQTT_PORT'),
-      { username: 'Nest CLient' },
+      { username: 'Nest Client' },
     );
-    this.client.on('connect', () => {
-      console.log(`Connected.`);
+    this.client.on('connect', async () => {
+      try {
+        //  새로 연결 시, 등록되어있는 모든 기기 구독
+        const devices = await this.deviceAll();
+
+        devices.forEach((item) => {
+          this.client
+            .subscribe(`${item.name}/#`, (err) => {
+              if (err) console.error(err);
+            })
+            .on('message', (topic, msg) => {
+              this.mqttGateway.publish(topic, Number(msg.toString()));
+            });
+        });
+      } catch (e) {
+        console.error(e);
+      }
     });
   }
-
   publishMessage(topic: string, msg: string) {
-    this.client.publish(topic, msg, (e) => {
+    //  아두이노는 너무 잘 끊겨서 qos 1로 함. retain 옵션으로 다시 구독하는 순간에 해당 메세지 전송받음
+    this.client.publish(topic, msg, { qos: 1, retain: true }, (e) => {
       if (e) console.error(e);
     });
   }
 
-  topicList(): Array<string> {
-    return Array.from(this.topics.keys());
+  async deviceAll() {
+    try {
+      return await this.prismaService.device.findMany();
+    } catch (e) {
+      console.error(e);
+    }
   }
-  topicRegister(topic: string) {
-    if (this.topics.has(topic)) return;
-    this.topics.set(topic, []);
-    this.client.subscribe(topic, (err) => {
-      console.log(`Registered ${topic}`);
-      if (err) {
-        console.error('Failed to subscribe to topic:', err);
+
+  async deviceNames(userId: number): Promise<string[]> {
+    try {
+      const res = await this.prismaService.userDevice.findMany({
+        where: {
+          userId,
+        },
+        include: {
+          device: true,
+        },
+      });
+      console.log(res);
+      const deviceNames: string[] = res.map((device) => device.device.name);
+      return deviceNames;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  async deviceRegister(userId: number, device: string) {
+    try {
+      //  없으면 생성
+      const res = await this.prismaService.device.upsert({
+        where: { name: device },
+        update: {},
+        create: {
+          name: device,
+          adminId: userId,
+        },
+      });
+      //  관계 생성
+      try {
+        await this.prismaService.userDevice.create({
+          data: {
+            userId,
+            deviceId: res.id,
+          },
+        });
+      } catch (e) {
+        console.error(e);
       }
-    });
-    this.client.on('message', (topic, msg) => {
-      this.mqttGateway.publish(topic, Number(msg.toString()));
-      console.log('on Message : ', topic, ', ', msg.toString());
-    });
+      return res.name;
+    } catch (e) {
+      console.error(e);
+    }
+    this.client
+      .subscribe(device, (err) => {
+        if (err) {
+          console.error('Failed to subscribe to topic:', err);
+        }
+      })
+      .on('message', (topic, msg) => {
+        this.mqttGateway.publish(topic, Number(msg.toString()));
+      });
+  }
+
+  async getDeviceInfo(id: number) {
+    try {
+      const res = await this.prismaService.device.findFirst({
+        where: {
+          id,
+        },
+      });
+      return res;
+    } catch (e) {
+      throw new NotFoundException(`No Device ${id}`);
+    }
   }
 }
