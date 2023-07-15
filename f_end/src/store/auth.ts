1d30bfff986a4e13f20a6b66e01a84dc067141ae
youngpar <youngseo321@gmail.com>
2023-07-15 11:14:55 +0900

    [FE: REFACTOR] Refactor to Composite API

--- f_end/src/store/auth.ts
+++ f_end/src/store/auth.ts
@@ -1,25 +1,26 @@
 import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 
-export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    token: '',
-  }),
-  actions: {
-    isAuthed(): boolean {
-      return this.token !== '';
-    },
-    parseCookies(): { [key: string]: string } {
-      return document.cookie
-        .split(';')
-        .reduce((cookies: { [key: string]: string }, cookie) => {
-          const [name, value] = cookie.split('=').map((c) => c.trim());
-          cookies[name] = value;
-          return cookies;
-        }, {});
-    },
-    parseToken(): void {
-      const cookies = this.parseCookies();
-      this.token = cookies[process.env.VUE_APP_TOKEN_NAME] || '';
-    },
-  },
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref('');
+  const isAuthed = computed(() => {
+    return token.value !== '';
+  });
+  function parseCookies(): { [key: string]: string } {
+    return document.cookie
+      .split(';')
+      .reduce((cookies: { [key: string]: string }, cookie) => {
+        const [name, value] = cookie.split('=').map((c) => c.trim());
+        cookies[name] = value;
+        return cookies;
+      }, {});
+  }
+  function parseToken(): void {
+    const cookies = parseCookies();
+    token.value = cookies[process.env.VUE_APP_TOKEN_NAME] || '';
+  }
+  function logout(): void {
+    token.value = '';
+  }
+  return { token, isAuthed, parseToken, logout };
 });
