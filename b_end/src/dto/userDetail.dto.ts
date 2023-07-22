25ff4cb3965099fb5a23cfbe3f67e8c265d856bd
youngpar <youngseo321@gmail.com>
2023-07-23 02:25:58 +0900

    [BE: FIX] UserDetailDto.devices => string[] & included

--- b_end/src/dto/userDetail.dto.ts
+++ b_end/src/dto/userDetail.dto.ts
@@ -1,8 +1,7 @@
 import { UserDto } from './user.dto';
-import { DeviceDto } from './device.dto';
 
 export class UserDetailDto extends UserDto {
   userEmail?: string;
   userImage?: string;
-  devices?: DeviceDto[];
+  devices?: string[];
 }
