1f9103a7b0de549c5a05102c5da310a15903eabe
youngpar <youngseo321@gmail.com>
2023-08-18 20:10:51 +0900

    [BE: DOCS] Set Not Required UserDetailDto

--- b_end/src/dto/userDetail.dto.ts
+++ b_end/src/dto/userDetail.dto.ts
@@ -1,7 +1,13 @@
 import { UserDto } from './user.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserDetailDto extends UserDto {
+  @ApiProperty({ required: false })
   userEmail?: string;
+
+  @ApiProperty({ required: false })
   userImage?: string;
+
+  @ApiProperty({ required: false })
   devices?: string[];
 }
