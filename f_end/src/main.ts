02bab456e12072115c64d7cc5386d88a9ea7ebac
youngpar <youngseo321@gmail.com>
2023-08-31 18:08:16 +0900

    [FE: REFACTOR] Axios to Global AxiosInstance

--- f_end/src/main.ts
+++ f_end/src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import axios from 'axios';
 
 const app = createApp(App);
+app.provide('$axios', axios.create());
 app.use(router);
 app.use(createPinia());
 app.mount('#app');
