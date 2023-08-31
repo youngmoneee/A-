cf4ea5b29a45164148e06248f177665d4e0c8336
youngpar <youngseo321@gmail.com>
2023-08-31 16:59:23 +0900

    [BE: REFACTOR] Remove Unused components

--- b_end/src/auth/auth.module.ts
+++ b_end/src/auth/auth.module.ts
@@ -2,22 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
-import { KakaoStrategy } from './strategy/kakao.strategy';
-import { GoogleStrategy } from './strategy/google.strategy';
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtGuard } from './guard/jwt.guard';
-import { AuthInterceptor } from './auth.interceptor';
 import { UserModule } from '../user/user.module';
 import { PrismaService } from '../prisma.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     ConfigModule,
     PassportModule.register({
       defaultStrategy: 'jwt',
-      strategies: [KakaoStrategy, GoogleStrategy, JwtStrategy],
+      strategies: [JwtStrategy],
     }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
@@ -30,16 +28,9 @@ import { PrismaService } from '../prisma.service';
       }),
     }),
     UserModule,
+    HttpModule,
   ],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    KakaoStrategy,
-    GoogleStrategy,
-    JwtStrategy,
-    JwtGuard,
-    AuthInterceptor,
-    PrismaService,
-  ],
+  providers: [AuthService, JwtStrategy, JwtGuard, PrismaService],
 })
 export class AuthModule {}
--- b_end/src/auth/guard/kakao.guard.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { OauthProvider } from '../../dto/enum.provider';
-
-@Injectable()
-export class KakaoGuard extends AuthGuard(OauthProvider.KAKAO) {}
