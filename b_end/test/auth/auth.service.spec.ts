c8f4e4f363e8022d0e593fd42d169503ec9e224b
youngpar <youngseo321@gmail.com>
2023-09-15 23:58:46 +0900

    [TEST: FEAT] Create Unit test File - User

---
+++ b_end/test/auth/auth.service.spec.ts
@@ -0,0 +1,50 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from '../../src/auth/auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { HttpService } from '@nestjs/axios';
+import { ROLE } from '../../src/dto/enum.role';
+
+describe('Auth Service Test', () => {
+  let authService: AuthService;
+  const mockUserRepository = {
+    findUserById: jest.fn(),
+    findUserByProvideId: jest.fn(),
+    createUser: jest.fn(),
+  };
+  const mockHttpService = {
+    post: jest.fn(),
+    get: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: 'Repository', useValue: mockUserRepository },
+        { provide: ConfigService, useValue: new ConfigService() },
+        { provide: JwtService, useValue: new JwtService() },
+        { provide: HttpService, useValue: new HttpService() },
+      ],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+  });
+
+  it('AuthService should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+
+  describe('userVerify Test', () => {
+    it('return true if user verified', async () => {
+      mockUserRepository.findUserById.mockResolvedValueOnce(true);
+      expect(
+        await authService.userVerify({
+          id: 1,
+          userRole: ROLE.USER,
+          userName: 'test',
+        }),
+      ).toBe(true);
+    });
+  });
+});
