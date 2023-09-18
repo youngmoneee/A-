cb9c06dc650b71342f30e216123d951217ac5ddf
youngpar <youngseo321@gmail.com>
2023-09-18 20:28:45 +0900

    [TEST: FEAT] Wrote Test Code

--- b_end/test/unit/user/user.service.spec.ts
+++ b_end/test/unit/user/user.service.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../../src/prisma.service';
-import { mockPrismaService } from './mocks/mockPrismaService';
-import { mockUserRepository } from './mocks/mockUserRepository';
+import { mockPrismaService } from './mocks/prisma.service.mock';
+import { mockUserRepository } from './mocks/user.repository.mock';
 import { UserService } from '../../../src/user/user.service';
+import { CreateUserDto } from '../../../src/dto/createUserDto';
+import { OauthProvider } from '../../../src/dto/enum.provider';
+import { ROLE } from '../../../src/dto/enum.role';
 
 describe('User Service Test', () => {
   let userService: UserService;
@@ -17,7 +20,30 @@ describe('User Service Test', () => {
 
     userService = module.get<UserService>(UserService);
   });
+
   it('User Service 생성 테스트', () => {
     expect(userService).toBeDefined();
+  });
+
+  describe('유저 상세 정보 조회 테스트', () => {
+    it('생성 후 조회 테스트', async () => {
+      const createUserDto: CreateUserDto = {
+        provider: OauthProvider.KAKAO,
+        userId: 'test',
+        userName: 'test',
+        userRole: ROLE.USER,
+        userImage: 'test-image',
+        userEmail: 'test-email',
+      };
+      const newUser = await mockUserRepository.createUser(createUserDto);
+      expect(
+        await mockUserRepository
+          .findUserDetailById(newUser.id)
+          .then((user) => user.id),
+      ).toEqual(newUser.id);
+    });
+    it('존재하지 않는 유저 조회 테스트', async () => {
+      expect(await mockUserRepository.findUserDetailById(99999)).toBe(null);
+    });
   });
 });
